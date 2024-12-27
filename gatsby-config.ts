b6f7f19025d1dc747049415f6aa30872ebf87e77
Michał Czapko <mczapko@gmail.com>
2024-12-27 21:57:15 +0100

    Update Gatsby config, change domain and project name

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `afazja.github.io`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `osw-afazja.github.io`,
+    siteUrl: `https://osw-afazja.github.io`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
